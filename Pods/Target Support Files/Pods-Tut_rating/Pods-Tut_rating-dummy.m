064edd5e617bc832e4c94e60fea535bc310be02d
Eray Alparslan <erayalparslan@yahoo.com>
2018-09-11 16:50:05 +0300

    iOS Notları

---
+++ Pods/Target Support Files/Pods-Tut_rating/Pods-Tut_rating-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tut_rating : NSObject
+@end
+@implementation PodsDummy_Pods_Tut_rating
+@end
--- Tut_rating/ViewController.swift
+++ Tut_rating/ViewController.swift
@@ -7,17 +7,16 @@
 //
 
 import UIKit
+import SwiftyStarRatingView
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var rateView: SwiftyStarRatingView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        
     }
 
 
